678a7d0610cb0460fc1c41428c5a55c690f90bc4
Millie Muteti <milliemuteti@gmail.com>
2023-10-25 12:42:30 +0300

    style the single todo cards

--- src/components/SingleToDo.tsx
+++ src/components/SingleToDo.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Todo } from './model';
+import {AiFillDelete, AiFillEdit } from 'react-icons/ai';
+import { MdOutlineDone } from 'react-icons/md';
+import "./style.css"
 
 type Props ={
     todo:Todo,
@@ -9,9 +12,24 @@ type Props ={
 
 const SingleToDo = ({toDos, setToDos, todo}: Props) => {
   return (
-    <div>
-      
-    </div>
+    <form className='todos__single'>
+        <span className='todos__single--text'>
+            {todo.toDo}
+
+        </span>
+        <div>
+            <span className='icon'>
+            <AiFillDelete />
+            </span>
+            <span className='icon'>
+                <AiFillEdit />
+            </span>
+            <span className='icon'>
+                <MdOutlineDone />
+            </span>
+        </div>
+
+    </form>
   )
 }
 
